96b5a3dc3f8295f2aaeecb94e71e13282fc824c1
DG <7-v-7@bk.ru>
2022-06-10 11:22:34 +0300

    2.2.1.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Main from '../main/main';
+import Card from '../card/card';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+// import Main from '../main/main';
+// import Favorites from '../favorites/favorites';
+// import Login from '../login/login';
+// import Offer from '../offer/offer';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
-
-const App = (props) => {
+function App(props) {
   const {quantityOffers} = props;
   return (
-    <Main
-      quantityOffers = {quantityOffers}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Card quantityOffers={quantityOffers} />
+        </Route>
+        {/* <Route exact path="/main" component={Main}>
+          <Main />
+        </Route>
+        <Route exact path="/favorites" component={Favorites}>
+          <Favorites />
+        </Route> */}
+        {/* <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/offer">
+          <Offer />
+        </Route> */}
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
-};
+}
 
 App.propTypes = {
   quantityOffers: PropTypes.number.isRequired,
